c2ef8387a1ac91dfff9c542eed5cd8b09dd99f8a
Julie <lecetre.julie@live.fr>
2018-03-28 12:45:26 +0200

    Add Sign/Login

---
+++ EpitechEisenhower/EpitechEisenhower/LoginViewController.swift
@@ -0,0 +1,9 @@
+//
+//  LoginViewController.swift
+//  EpitechEisenhower
+//
+//  Created by Julie Lecêtre on 28/03/2018.
+//  Copyright © 2018 Teddy Carneiro. All rights reserved.
+//
+
+import Foundation
